e0ec50be3b9ec76c4445af1e954f8e85d678b55b
Dmitriy Vasilyev <61234128+d-svtch@users.noreply.github.com>
2025-02-25 09:32:43 +0300

    fix: moved creating and deleting archive to fixture

--- conftest.py
+++ conftest.py
@@ -1,10 +1,22 @@
 import os
+import shutil
 
-if not os.path.exists("archives"):
-    os.mkdir("archives")
+import pytest
+from zipfile import ZipFile, ZIP_DEFLATED
 
 CURRENT_DIR = os.path.dirname(__file__)
 ARCHIVE_DIR = os.path.join(CURRENT_DIR, "archives")
 FILES_DIR = os.path.join(CURRENT_DIR, "files")
 ZIP_PATH = os.path.join(ARCHIVE_DIR, "test_zip.zip")
 
+
+@pytest.fixture(scope="session", autouse=True)
+def operations_with_archive():
+    if not os.path.exists("archives"):
+        os.mkdir("archives")
+    with ZipFile(os.path.join(ARCHIVE_DIR, "test_zip.zip"), mode='w', compression=ZIP_DEFLATED) as zip_file:
+        for file in os.listdir(FILES_DIR):
+            add_file = os.path.join("files", file)
+            zip_file.write(add_file)
+    yield
+    shutil.rmtree(ARCHIVE_DIR)
--- test_homework.py
+++ test_homework.py
@@ -1,15 +1,8 @@
-import os
-from zipfile import ZipFile, ZIP_DEFLATED
+from zipfile import ZipFile
 from pypdf import PdfReader
 from openpyxl import load_workbook
-from conftest import ARCHIVE_DIR, FILES_DIR, ZIP_PATH
-import shutil
+from conftest import ZIP_PATH
 
-def test_creating_archive():
-    with ZipFile(os.path.join(ARCHIVE_DIR, "test_zip.zip"), mode='w', compression=ZIP_DEFLATED) as zip_file:
-        for file in os.listdir(FILES_DIR):
-            add_file = os.path.join("files", file)
-            zip_file.write(add_file)
 
 def test_pdf_reader():
     with ZipFile(ZIP_PATH, "r") as zip_file:
@@ -17,23 +10,16 @@ def test_pdf_reader():
             reader = PdfReader(pdf_file)
             assert "This is pdf file for tests" == reader.pages[0].extract_text()
 
+
 def test_xlsx_reader():
     with ZipFile(ZIP_PATH, "r") as zip_file:
         with zip_file.open("files/test_xlsx.xlsx") as xlsx_file:
             reader = load_workbook(xlsx_file)
             assert "TEST" == reader.active.cell(row=3, column=2).value
 
+
 def test_csv_reader():
     with ZipFile(ZIP_PATH, "r") as zip_file:
         with zip_file.open("files/test_csv.csv") as csv_file:
             reader = csv_file.read().decode("utf-8")
             assert "Jenkins" in reader
-
-def test_deleting_archives_directory():
-    shutil.rmtree(ARCHIVE_DIR)
-
-
-
-
-
-
